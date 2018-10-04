6b7d2ce9809bfaa39444149d31dd5e728fb2fbae
Elena Kulinich <elmatthw8@gmail.com>
2018-10-04 10:23:36 +0300

    added MyException

--- src/training/entity/Participant.java
+++ src/training/entity/Participant.java
@@ -1,5 +1,33 @@
 package training.entity;
 
-public class Participant {
+import java.util.Date;
+
+public class Participant extends User{
+	private User user;
+	private Date currentDate;
+	private Event event;
+	
+	Participant (){
+		super();
+	}
+	
+	public void registerOnEvent() throws MyException {
+		currentDate = new Date();
+		if(currentDate.after(event.getDate())) {
+			throw new MyException();
+		}
+		else {
+			event.getListOfParticipants().add(this);
+		}
+	}
+	
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+	
+	public Event getEvent() {
+		return event;
+	}
+>>>>>>> added MyException
 
 }
